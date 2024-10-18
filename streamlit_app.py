157661f909c2eadc836b47c3c7d5bbebbdd4e585
HulaHoopoe <hoopoe45.89@gmail.com>
2024-10-18 19:01:12 +0500

    Update streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,50 +5,16 @@ df = pd.read_csv('data/titanic_train.csv', delimiter = ',')
 
 st.image('data/14047.webp')
 st.title("Подсчет количества погибших детей")
+
+df.drop_duplicates()
+df = df.reset_index(drop=True)
+
+age = st.number_input("Enter age:", min_value=0, max_value=18, value=10, step=1)
+
+if st.button("Подсчитать"):
+    children_died = df[(df['Age'] <= age) & (df['Survived'] == 0)]
+    result = children_died.groupby('Embarked').size().reset_index(name='Count')
+
+    st.write(result)
+
 st.dataframe(df)
-
-# import csv
-# import json
-
-
-# with open('/titanic_train.csv') as f:
-#   reader = csv.reader(f)
-
-#   headers = next(reader)
-#   embarks = {}
-
-#   for row in reader:
-#     id = row[0]
-#     survived = row[1]
-#     age = row[5]
-#     embarked = row[11]
-#     if not age or not embarked:
-#       continue
-
-#     if int(float(age)) > 18 or int(float(age)) == 0 or survived == '1':
-#       continue
-
-#     # print(f'{id} | {age} | {embarked}')
-
-#     # if embarked not in embarks:
-#     #   embarks[embarked] = [age]
-#     # else:
-#     #   embarks[embarked] += [age]
-
-#     if embarked not in embarks:
-#       embarks.update({embarked: 1})
-#     else:
-#       embarks.update({embarked: embarks[embarked] + 1})
-
-# result = {
-#     "embarked": []
-# }
-
-# for key in embarks:
-#   res = {"name" : key, "survived_children": embarks[key]}
-#   result["embarked"].append(res)
-
-# with open('data.json', 'w') as file:
-#   json.dump(result, file, indent=4)
-
-# json.dumps(result)
